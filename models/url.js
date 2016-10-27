7a52c9e0505de09c6f17dc0024b4c629e59e839c
gjdickens <geeregg@gmail.com>
2016-10-27 07:56:12 +0100

    basic functionality complete

--- models/url.js
+++ models/url.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 
 var UrlSchema   = new Schema({
     original_url: String,
-    short_url: String
+    short_url: String,
+    short_url_code: String
 });
 
 module.exports = mongoose.model('Url', UrlSchema);
--- server.js
+++ server.js
@@ -24,9 +24,12 @@ var port = process.env.PORT || 8080;
 
 // Set Up Routes
 var router = express.Router();
+var routerFind = express.Router();
 
 // Middleware for all requests
 router.use(function(req, res, next) {
+  req.data = req.url.slice(1);
+  req.url ='/';
   next();
 });
 
@@ -35,26 +38,46 @@ router.use(function(req, res, next) {
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 
+router.route('/')
 
-router.route('/:url_input')
-
-  //create a bear
+  //create a new url
   .get(function(req, res) {
 
-    var url = new Url();
-    url.original_url = req.params.url_input;
-    url.short_url = Math.random().toString(36).substr(2, 5);
+      //http://stackoverflow.com/questions/8188645/javascript-regex-to-match-a-url-in-a-field-of-text
+      var urlPattern = new RegExp("(http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?")
+      var url = new Url();
 
-    url.save(function(err) {
-      if (err) {res.send(err)};
-      res.json({ original_url: url.original_url, short_url: url.short_url });
-    });
+      if (urlPattern.test(req.data)) {
+        url.original_url = req.data;
+        console.log(req.data);
+        url.short_url_code = Math.random().toString(36).substr(2, 5);
+        url.short_url = 'https://gj-url-shortener.herokuapp.com/' + url.short_url_code;
+
+        url.save(function(err) {
+          if (err) {res.send(err)};
+          res.json({ original_url: url.original_url, short_url: url.short_url, short_url_code: url.short_url_code });
+        });
+      }
+      else {
+        res.json({ message: "Please enter a valid url" });
+      }
+
   });
 
+  routerFind.route('/:short_url')
+    //create a new url
+    .get(function(req, res) {
+      Url.find( { short_url_code: req.params.short_url }, function(err, url) {
+        if (err) {res.send(err)};
+        res.redirect(url[0].original_url);
+      });
+
+    });
 
 
 // Register Routes
-app.use('/api', router);
+app.use('/new', router);
+app.use('/find', routerFind);
 
 // Start Server
 app.listen(port);
